dbc40cddccf1d20d5858aa7ef86688044aeb819a
Kevin Rivas <KRivas@dacgroup.com>
2023-10-23 04:08:05 -0400

    update

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,7 +4,20 @@ Console.WriteLine("sup");
 Console.WriteLine("sup2");
 Console.WriteLine("sup3");
 
+for (int i = 0; i < args.Length; i++)
+{
+    Console.WriteLine($"Parameter {i + 1}: {args[i]}");
+}
 
+Console.WriteLine("sup3");
+Console.WriteLine("sup3");
+Console.WriteLine("sup3");
+Console.WriteLine("sup3");
+
+Console.WriteLine("sup3");
+Console.WriteLine("sup3");
+Console.WriteLine("sup3");
+Console.WriteLine("sup3");
 
 
 
